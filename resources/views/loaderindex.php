4f206419c0b7e166b74c111001e184745d36fec6
ByronCarrion <carrionmejia756@gmail.com>
2019-11-10 04:10:26 -0600

    Se dio estiolo a la pagina principal

---
+++ resources/views/loaderindex.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="/public/css/estilo.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id='loading-wrapper'>
+  <div id='loading-center' class='loading-center'></div>
+  <div id='loading-fill' class='loading-fill'></div>
+  <div id='loading-alt-center' class='loading-center'></div>
+  <div id='loading-alt-fill' class='loading-fill'></div>
+</div>
+
+
+  </body>
+</html>
