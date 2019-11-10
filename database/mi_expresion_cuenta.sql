67a117565af9ca781d284a0609a3978fd1a0790c
jerielmm <56746042+jerielmm@users.noreply.github.com>
2019-11-09 19:00:28 -0600

    feat(base de datos) : se reestructuro las tablas, se crearon codigo de almacenamiento de datos en tablas

--- database/mi_expresion_cuenta.sql
+++ database/mi_expresion_cuenta.sql
@@ -1,28 +1,30 @@
 create database if not exists expresioncuenta;
-use  expresioncuenta;
+use expresioncuenta;
 create table if not exists login
 (
 usuario varchar(25) not null,
 correo varchar(50) not null,
-contrasena varchar(8) not null,
+pasword varchar(8) not null,
 primary key (usuario)
 );
 create table if not exists nivel
 (idnivel int not null,
 nombrenivel varchar(25),
-primary key (idnivel)
+usuario varchar(50),
+primary key (idnivel),
+foreign key (usuario) references login(usuario) 
 ) ;
 create table if not exists actividades
 (idacti int not null,
 nombreacti varchar(25),
-nota int,
 idnivel int not null,
+primary key (idacti),
 foreign key (idnivel) references nivel(idnivel)
 );
 create table if not exists evaluacion
 (idevaluacion int not null,
-nombreacti varchar(25),
+idacti int not null,
 nota int,
-usuario varchar(25) not null,
-foreign key (usuario) references login(usuario)
+primary key (idevaluacion),
+foreign key (idacti) references actividades(idacti)
 );
