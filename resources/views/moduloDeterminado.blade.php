ddcdd1da5973b4313e01b22380ed8f1d7e58626e
Jehaann Manzanares <jehaann07carlos@gmail.com>
2020-05-25 02:36:53 -0600

    update ( Modulo determinado ) : actulizando imagenes de modulos

--- resources/views/moduloDeterminado.blade.php
+++ resources/views/moduloDeterminado.blade.php
@@ -4,25 +4,21 @@
 <section class="container-menu">
     <h1 class="title">Módulo determinantes</h1>
     <div class="menu-item">
-        <a href="/Actividades"  class="container-item">
-            <img class="icon-menu" src="/images/Icons_Modulo_1/determinantes3.svg">
-            <p class="subtitle-menu">Artículos</p>
+        <a href="/Actividades"  class="container-item articulos">
+            <p class="title-item">Artículos</p>
         </a>
     </div>
     <div class="menu-item">
-        <a href="" class="container-item">
-            <img class="icon-menu" src="/images/Icons_Modulo_1/determinantes3.svg">
-            <p class="subtitle-menu">Demostrativo</p>
+        <a href="" class="container-item demostrativo">
+            <p class="title-item">Demostrativo</p>
         </a>
-        <a href="" class="container-item">
-            <img class="icon-menu" src="/images/Icons_Modulo_1/determinantes3.svg">
-            <p class="subtitle-menu">Posesivo</p>
+        <a href="" class="container-item posesivo">
+            <p class="title-item">Posesivo</p>
         </a>
     </div>
     <div class="menu-item">
-        <a href="/resources/views/ModuloDeterminante.blade.php"  class="container-item">
-            <img class="icon-menu" src="/images/Icons_Modulo_1/determinantes3.svg">
-            <p class="subtitle-menu">Numerales</p>
+        <a href="/resources/views/ModuloDeterminante.blade.php"  class="container-item numerales">
+            <p class="title-item">Numerales</p>
         </a>
     </div>
 
