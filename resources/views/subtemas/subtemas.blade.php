751bf77a6dd81d8fa0e54e42c06f43d5d2a8da5c
Jehaann Manzanares <jehaann07carlos@gmail.com>
2020-07-19 23:14:16 -0600

    update ( subtemas.balde.php ): mejorando estructura html y estilos para listar de forma dinamica los subtemas traidos de la BD

--- resources/views/subtemas/subtemas.blade.php
+++ resources/views/subtemas/subtemas.blade.php
@@ -3,16 +3,28 @@
 @section('content')
 @include('HeaderActivity')
 
-<section class="container-menu">
+
 @foreach($temas as $tema)
+<section class="container-menu">
     <h1 class="title">Tema {{$tema->nombre}}</h1>
-    @foreach($subtema as $subtema)
     <div class="menu-item">
-        <a href="/Actividades"  class="container-item articulos">
+    @foreach($subtema as $subtema)
+        <div class="container-item determinante">
             <p class="title-item">{{$subtema->nombre}}</p>
-        </a>
-    </div>
+            <span class="subtitle-item">4 clases</span>
+            <div class="mimi-modal">
+                <a href="/ActividadesAprender" class="container-aprender">
+                    <img class="icon-aprender" src="/images/icon_aprender.svg" alt="Aprender">
+                    <p>Aprender</p>
+                </a>
+                <a href="" class="container-practicar">
+                    <img class="icon-practicar" src="/images/icon_practicar.svg" alt="Practicar">
+                    <p>Practicar</p>
+                </a>
+            </div>
+        </div>
     @endforeach
-@endforeach
+    </div>
 </section>
+@endforeach
 @endsection
