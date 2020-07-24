2bc78af0b123b22ab4debe47b6e0e8e19ba30522
Jehaann-Manzanares <jehaann07carlos@gmail.com>
2020-07-24 16:30:31 -0600

    iniciando controlador

---
+++ app/Actividades.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MiExpresionCuenta;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Actividades extends Model
+{
+    //
+}
---
+++ app/Http/Controllers/ActividadesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MiExpresionCuenta\Http\Controllers;
+
+use Illuminate\Http\Request;
+use MiExpresionCuenta\Actividades;
+use MiExpresionCuenta\Opciones;
+
+
+class ActividadesController extends Controller
+{
+    
+}
