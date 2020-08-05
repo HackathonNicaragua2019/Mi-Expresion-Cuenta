e85aaf50e59e650f3889f88442a09fc8bed0d881
ByronCarrion <carrionmejia756@gmail.com>
2020-08-05 05:03:25 -0600

    Update(Controller and Model Teoria) Se modifico el modelo y los controller para retornar informacion de los subtemas

--- app/Http/Controllers/TeoriaController.php
+++ app/Http/Controllers/TeoriaController.php
@@ -5,23 +5,41 @@ namespace MiExpresionCuenta\Http\Controllers;
 use Illuminate\Http\Request;
 use MiExpresionCuenta\Teoria;
 use MiExpresionCuenta\Temas;
+use MiExpresionCuenta\Subtemas;
 
 class TeoriaController extends Controller
 {
 
-    public function index($idteoria)
+    public function TeoriaTemas($idteoria)
     {
+
+        $temas = "temas";
         
         $teoria= Teoria::with(['teoria' => function ( $query ){
-            $query->with(['temas'] );
-            
+            $query->with(['temas'] );            
         }])
         ->whereHas('teoria')
         ->where('id','=', $idteoria)
-        ->get();
-        
+        ->get(); 
         return view('ActividadesAprender',compact('teoria'));
     }
     
+
+    public function TeoriaSubtemas($subtema, $idsubtema)
+    {
+        $teoria= Teoria::with(['teoriasubtemas' => function ( $query ){
+            $query->with(['Subtemas'] );
+            
+        }])
+        ->whereHas('teoriasubtemas')
+        ->where('id','=', $idsubtema)
+        ->get();
+        
+        return view('ActividadesAprender',compact('teoria'));
+
+    }
     
+
+
+
 }
--- app/Teoria.php
+++ app/Teoria.php
@@ -26,6 +26,12 @@ class Teoria extends Model
  public function teoria()
  {
      return $this->hasMany(Temas::class, 'idteoria', 'id');
+     
  }
+ public function teoriasubtemas()
+ {
+     return $this->hasMany(Subtemas::class, 'idteoria', 'id');
+ }
+
 
 }
