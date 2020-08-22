77d4c5c29cc21b8fd682754646a2ba32e8125f3e
Jehaann Manzanares <jehaann07carlos@gmail.com>
2020-08-22 11:51:59 -0600

    fix ( headerActivity ): mejorando stylos, para agregar efecto hover y mostrar pistas en los temas

--- public/js/app.js
+++ public/js/app.js
@@ -19,3 +19,58 @@ items.addEventListener('click', function() {
     subitems.classList.toggle("active")
   });
 
+const containeraprender = document.getElementsByClassName("container-aprender");
+const containerpracticar = document.getElementsByClassName("container-practicar");
+const contentpistas = document.querySelector('.content-pistas');
+const idea = document.querySelector('.idea');
+const iconpistas = document.querySelector('#icon-pistas');
+const textopistas = document.querySelector('#texto-pistas');
+
+/* HOVER BOTON APRENDER */
+for (let i = 0; i < containeraprender.length; i++) {
+        containeraprender[i].addEventListener('mouseover', () => {
+        contentpistas.classList.add('active')
+        idea.classList.add('active')
+        /*firefox*/
+        idea.src ='/images/icon_idea_hover.svg';
+        iconpistas.src = '/images/icon_aprender.svg';
+        textopistas.innerHTML = 'Selecciona el botón <strong>Introducción</strong> para informarte sobre el tema.'
+      })
+      
+      containeraprender[i].addEventListener('mouseout', () => {
+        contentpistas.classList.remove('active')
+        idea.classList.remove('active')
+        /*firefox*/
+        idea.src = '/images/icon_idea.svg';
+      })
+  
+}
+
+/* HOVER BOTON PRACTICAR */
+for (let i = 0; i < containerpracticar.length; i++) {
+  containerpracticar[i].addEventListener('mouseover', () => {
+  contentpistas.classList.add('active')
+  idea.classList.add('active')
+  /*firefox*/
+  idea.src ='/images/icon_idea_hover.svg';
+  iconpistas.src = '/images/icon_practicar.svg';
+  textopistas.innerHTML = 'Accede a los <strong>subtemas</strong> de actividades prácticas.'
+})
+
+  containerpracticar[i].addEventListener('mouseout', () => {
+  contentpistas.classList.remove('active')
+  idea.classList.remove('active')
+  /*firefox*/
+  idea.src = '/images/icon_idea.svg';
+})
+
+}
+
+idea.addEventListener('mouseover', ()=>{
+  idea.src ='/images/icon_idea_hover.svg';
+})
+
+idea.addEventListener('mouseout', ()=>{
+  idea.src ='/images/icon_idea.svg';
+})
+
--- resources/views/subtemas/subtemas.blade.php
+++ resources/views/subtemas/subtemas.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.app')
 @section('content')
-
-
 <header class="header activity">
     <a href="/home" class="icons-container">
         <img class="logo-header" src="/images/icon_back.svg"/>
         <span class="atras">Atras</span>
     </a>
-    <a href="" class="icons-container" style="display:none">
-        <span class="pistas">Pistas</span>
-        <img class="logo-header idea" src="/images/icon_idea.svg"/>
-    </a>
+    <div class="icons-container">
+        <div class="icons">
+            <span class="pistas">Pistas</span>
+            <img class="logo-header idea" src="/images/icon_idea.svg"/>
+        </div>
+        <div class="content-pistas" id="pistas">
+            <img id="icon-pistas" class="icon-aprender" src="/images/icon_aprender.svg" alt="Aprender">
+            <p id="texto-pistas">Selecciona el botón <strong>Aprender</strong> para obtener información sobre un subtema</p>
+        </div>
+    </div>
 </header>
 @foreach($temas as $tema)
 <section class="container-menu">
@@ -20,11 +24,11 @@
         <div class="container-item" style="background-image: url('{{$subtema->imagen}}'">
             <p class="title-item">{{$subtema->nombre}}</p>
             <div class="mimi-modal">
-                <a href="/ActividadesAprender/{{$subtema->nombre}}/{{$subtema->idteoria}}" class="container-aprender">
+                <a id="btn-aprender" href="/ActividadesAprender/{{$subtema->nombre}}/{{$subtema->idteoria}}" class="container-aprender btn-aprender" >
                     <img class="icon-aprender" src="/images/icon_aprender.svg" alt="Aprender">
                     <p>Aprender</p>
                 </a>
-                <a href="/actividad{{$subtema->id}}" class="container-practicar">
+                <a id="btn-practicar" href="/actividad{{$subtema->id}}" class="container-practicar btn-practicar">
                     <img class="icon-practicar" src="/images/icon_practicar.svg" alt="Practicar">
                     <p>Practicar</p>
                 </a>
